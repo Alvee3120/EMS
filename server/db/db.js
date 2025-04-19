aeb7291329d14c2f5504f251d21f58ead6364c8c
fazlahkarim2002@gmail.com <p@ssword.@lvee55>
2025-04-19 22:02:10 +0600

    login error sloved

--- server/db/db.js
+++ server/db/db.js
@@ -1,17 +1,3 @@
-// import mongoose from "mongoose";
-
-// const connectToDatabse = async () => {
-
-//     try{
-//         await mongoose.connect(process.env.MONGODB_URL)
-//     } catch (error){
-//         console.log(error)
-        
-//     }
-// }
-
-// export default connectToDatabse;
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -19,7 +5,9 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://fazlahalvee20:LFf3YpJkcUQncaJu@cluster0.txqngea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Database connection failed:", error);
